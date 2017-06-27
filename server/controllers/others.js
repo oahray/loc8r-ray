c9b32d7d5152c83bcab3cc2975e4a8f892ce923c
Oahray <rodneyoare@gmail.com>
2017-06-27 15:36:31 +0100

    Move data away from views into controllers

--- server/controllers/others.js
+++ server/controllers/others.js
@@ -1,7 +1,7 @@
 var about = function(req, res) {
   res.render('generic-text', {
     title: 'About Loc8r-Ray',
-    content: "| Loc8r was created to help people find places to sit down and get a bit of work done. \n\n\ Created for your convenience."
+    content: "Loc8r-Ray was created to help people find places to sit down and get a bit of work done. \n\n\ Created for your convenience."
   });
 };
 
